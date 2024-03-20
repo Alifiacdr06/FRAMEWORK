43d26b4fb0e51746f4683cceda86145e1761d403
Alifiacdr06 <164138152+Alifiacdr06@users.noreply.github.com>
2024-03-20 13:14:26 +0700

    Add files via upload

---
+++ storage/framework/views/171f1831f42f7288fe0c6cda748812fd.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>Web Personal</title>
+<link rel="stylesheet"
+href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/css/bootstrap.min.
+css">
+<link rel="stylesheet"
+href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.6.0/font/bootstrap-icons.css
+">
+<link rel="stylesheet" href="../css2/styles.css">
+</head>
+<body>
+    <!-- Navbar -->
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+        <div class="container-fluid">
+          <a class="navbar-brand" href="#"></a>
+            <img src="../image/gemini_logo.png" alt="logo" style="width: 40px;">
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarNavDropdown">
+            <ul class="navbar-nav">
+              <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#" style="color: white;">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" style="color: white;">About</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="#" style="color: white;" >Portofolio</a>
+              </li>
+              <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false" style="color: white;">
+                  Contact
+                </a>
+                <ul class="dropdown-menu">
+                  <li><a class="dropdown-item" href="#">Instagram</a></li>
+                  <li><a class="dropdown-item" href="#">Whatsapp</a></li>
+                  <li><a class="dropdown-item" href="#">Twitter</a></li>
+                </ul>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+        <!-- Main -->
+<div class="bg-light mt-5" id="main">
+    <!-- Container -->
+    <div class="container py-5 px-4">
+        <div class="row">
+            <div class="col-md-5 order-md-2">
+                <img class="img-fluid" src="../image/logo.png.png" alt="mainlogo">
+            </div>
+        <div class="col-md-7 order-md-1">
+         <h1 class="mt-4 display-3" style="font-size:x-large; font-family:'Times New Roman', Times, serif; font-weight: bold;" >Hi There,</h1>
+         <h2 class="mt-4 display-3" style="font-family: Impact, Haettenschweiler, 'Arial Narrow Bold', sans-serif;">I am Alifia Chandra</h2>
+        <p class="fs-5 mt-3">I'm usually called Fia. I come from Jember and I am currently studying in Surabaya City. I am 20 years old and currently studying at Telkom University Surabaya.</p>
+        <div class="row">
+            <div class="d-flex flex-column flex-md-row">
+                <button type="button" class="btn bg-body-tertiary text-white btn-lg mb-3 me-md-3 px-4 py-3">Get More Intoduction</button>
+             </div>
+        </div>
+        </div>
+        </div>
+        </div>
+    </div>
+
+    <!-- Footer -->
+<div id="footer" class="bg-light py-5">
+    <!-- Container -->
+    <div class="container py-5 px-4">
+        <div class="row">
+            <div class="col-lg-3 mb-5">
+                <a href="#" class="logo text-decoration-none">
+                    <div class="d-flex">
+                        <img class="img-fluid" src="../image/gemini_logo.png"
+                    alt="Bootstrap Logo" style="width: 40px;">
+                    <div class="fs-5 ms-2 text-black">Gemini Energy</div>
+                </div>
+            </a>
+            <div class="mt-2 text-muted">
+                <small>People who have the zodiac sign Gemini with all forms of traits and personalities <a href="#" class="text-black" style="font-weight: bold;">Gemini vibes</a> with the help of <a
+                 href="#" class="text-black">our personalities</a>.</small>
+            </div>
+            
+        </div>
+        <div class="col-6 col-lg-2 offset-lg-1 mb-5">
+            <h5>Home</h5>
+            <ul class="list-unstyled">
+                <li class="mb-2"><a href="#">Home</a></li>
+                <li class="mb-2"><a href="#">Docs</a></li>
+                <!-- <li class="mb-2"><a href="#">Examples</a></li>
+                <li class="mb-2"><a href="#">Icons</a></li>
+                <li class="mb-2"><a href="#">Themes</a></li> -->
+                <li class="mb-2"><a href="#">Blog</a></li>
+                <!-- <li class="mb-2"><a href="#">Swag Store</a></li> -->
+            </ul>
+        </div>
+        <div class="col-6 col-lg-2 mb-5">
+        <h5>About</h5>
+            <ul class="list-unstyled">
+                <li class="mb-2"><a href="#">Personality</a></li>
+                <li class="mb-2"><a href="#">Experience</a></li>
+                <!-- <li class="mb-2"><a href="#">Webpack</a></li>
+                <li class="mb-2"><a href="#">Parcel</a></li> -->
+            </ul>
+        </div>
+        <div class="col-6 col-lg-2 mb-5">
+            <h5>Portofolio</h5>
+            <ul class="list-unstyled">
+                <li class="mb-2"><a href="#">Picture</a></li>
+                <li class="mb-2"><a href="#">Music</a></li>
+                <!-- <li class="mb-2"><a href="#">Icons</a></li>
+                <li class="mb-2"><a href="#">RFS</a></li>
+                <li class="mb-2"><a href="#">npm starter</a></li> -->
+            </ul>
+        </div>
+        <div class="col-6 col-lg-2 mb-5">
+                <h5>Contact</h5>
+                <ul class="list-unstyled">
+                    <li class="mb-2"><a href="#">Instagram</a></li>
+                    <li class="mb-2"><a href="#">Whatsapp</a></li>
+                    <li class="mb-2"><a href="#">Twitter</a></li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
+<script
+src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha2/dist/js/bootstrap.bundle
+.min.js"></script>
+</body>
+</html>
+<?php /**PATH C:\xampp\htdocs\basic-route\resources\views/fia.blade.php ENDPATH**/ ?>
